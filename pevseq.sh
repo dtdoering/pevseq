359a644ff1b65856e98ccb8a90183ea50494d784
Drew Doering <dtdoering@wisc.edu>
2016-08-18 14:53:42 -0500

    parse IN, HOMOLOGSFILE, and OUT arguments

--- pevseq.sh
+++ pevseq.sh
@@ -1,31 +1,45 @@
 #!/bin/sh
 
-IN=$1 # input data directory (merged reads)
-OUT=$2 # destination directory (assembled FASTA, BLAST results, )
+##
+# Run from directory containing merged reads, aka $IN
+# Ex (Scarcity): /home/GLBRCORG/dtdoering/seqdata/merged/ARRPA
+##
+
+IN=$1 # input data directory (assembled reads)
+HOMOLOGSFILE= $2 # FASTA filepath with input homologs/variants
+OUT=$3 # destination directory (assembled FASTA, BLAST results, )
+
+echo "Working directory: ""$IN"
+echo "Input homologs: ""$HOMOLOGSFILE"
+echo "Output directory: ""$OUT"
 
 # create array of homologs for later comparison
-HOMS=($(grep ">" ATX1_Homologs_Pilot.fasta | awk '{ gsub(">","",$1); print $1 }'))
+HOMS=($(grep ">" $HOMOLOGSFILE | awk '{ gsub(">","",$1); print $1 }'))
 
 i=1
 while [ $i -le 59 ]; do
-  EXPT=$(ls -al $HOME/seqdata/merged/ARRPA | grep -Po "\b"$i"_[ATGC-]*" | head -1)
+  EXPT=$(ls -al $IN/assembled | grep -Po "\b"$i"_[ATGC-]*" | head -1)
 
   # merged FASTQ -> FASTA with seqtk
-  seqtk seq -a "$EXPT".assembled.fastq > "$EXPT".assembled.fasta
+  seqtk seq -a $IN/assembled/"$EXPT".assembled.fastq > $IN/assembled/"$EXPT".assembled.fasta
+
+  # create BLAST db if needed
+  if [ ! -f $(dirname $HOMOLOGSFILE)/blastdbs/$(basename $HOMOLOGSFILE).nhr ]; then
+    makeblastdb -in $HOMOLOGSFILE -dbtype 'nucl' -out $(dirname $HOMOLOGSFILE)/blastdbs/$(basename $HOMOLOGSFILE)
+  fi
 
   # BLAST each read against the input homologs
-  blastn -query "$EXPT".assembled.fasta -db ATX1_Homologs_Pilot.fasta -max_hsps 1 -max_target_seqs -out "$EXPT".tsv -outfmt 6
+  blastn -query $IN/assembled/"$EXPT".assembled.fasta -db $(dirname $HOMOLOGSFILE)/blastdbs/$(basename $HOMOLOGSFILE) -max_hsps 1 -max_target_seqs -out $OUT/blastresults/"$EXPT".blastout.tsv -outfmt 6
 
   # count the number of each homolog
   for j in $HOMS; do
-    READS=$(grep $j "$EXPT".tsv | wc -l)
-    TOTAL=$(wc -l < "$EXPT".tsv)
+    READS=$(grep $j $OUT/blastresults/"$EXPT".blastout.tsv | wc -l)
+    TOTAL=$(wc -l < $OUT/blastresults/"$EXPT".blastout.tsv)
+
     # Output like "Scer: X / TOT reads (Y.YYY %)"
-    echo "$j: "$READS" / "$TOTAL" reads ($(echo "scale=3;100*$READS/$TOTAL" | bc) %)" 2>&1 | tee -a "$EXPT"_counts.txt
+    echo "$j: "$READS" / "$TOTAL" reads ($(echo "scale=3;100*$READS/$TOTAL" | bc) %)" 2>&1 | tee -a $OUT/"$EXPT"_counts.txt
   done
 
   echo "Done with "$EXPT""
   ((i++))
 done
-
-# output results to R-friendly file(s)
