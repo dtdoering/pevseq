ac0ea59c223645c3257ca63ee7678db91501842b
Drew Doering <dtdoering@wisc.edu>
2016-08-19 12:44:59 -0500

    Add verbose output, change some echo statements to printf, and output all counts to R-friendly CSV file.

--- pevseq.sh
+++ pevseq.sh
@@ -25,25 +25,33 @@ while [ $i -le 59 ]; do
   EXPT=$(ls -al $IN/assembled | grep -Po "\b"$i"_[ATGC-]*" | head -1)
 
   # merged FASTQ -> FASTA with seqtk
+  echo "  creating FASTA..."
   seqtk seq -a $IN/assembled/"$EXPT".assembled.fastq > $IN/assembled/"$EXPT".assembled.fasta
 
   # create BLAST db if needed
   if [ ! -f $(dirname $HOMOLOGSFILE)/blastdbs/$(basename $HOMOLOGSFILE).nhr ]; then
+    echo "  making blastdb..."
     makeblastdb -in $HOMOLOGSFILE -dbtype 'nucl' -out $(dirname $HOMOLOGSFILE)/blastdbs/$(basename $HOMOLOGSFILE)
   fi
 
   # BLAST each read against the input homologs
+  echo "  BLASTing..."
   blastn -query $IN/assembled/"$EXPT".assembled.fasta -db $(dirname $HOMOLOGSFILE)/blastdbs/$(basename $HOMOLOGSFILE) -max_target_seqs 1 -out $OUT/blastresults/"$EXPT".blastout.tsv -outfmt 6
 
   # count the number of each homolog
+  echo "  creating counts..."
   for j in $HOMS; do
     READS=$(grep $j $OUT/blastresults/"$EXPT".blastout.tsv | wc -l)
     TOTAL=$(wc -l < $OUT/blastresults/"$EXPT".blastout.tsv)
 
     # Output like "Scer: X / TOT reads (Y.YYY %)"
-    echo "$j: "$READS" / "$TOTAL" reads ($(echo "scale=3;100*$READS/$TOTAL" | bc) %)" 2>&1 | tee -a $OUT/"$EXPT"_counts.txt
+    echo "$j: "$READS" / "$TOTAL" reads ($(echo "scale=3;100*$READS/$TOTAL" | bc) %)" | tee -a $OUT/"$EXPT"_counts.txt
+
+    printf "%s,%s,%s,%s\n" "$EXPT" "$j" "$READS" "$TOTAL" > $IN/output/$(basename $IN)_counts.csv
+
   done
 
-  echo "Done with "$EXPT""
+  echo "Done with $EXPT"
+  echo ""
   ((i++))
 done
